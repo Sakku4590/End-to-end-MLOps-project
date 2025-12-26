ff9ea1c03f791620081a192ffbef3d84d89f8f93
Sakku4590 <ssklinemustaque3@gmail.com>
2025-12-26 19:10:22 +0530

    notebook created and model is ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="sakline mustaque",
+    author_email="saklinemustaque5@gmail.com",
+    packages=find_packages()
+)
